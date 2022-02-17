4165d9a5b077ccb34358fe616c5c7fdc3bb61081
moh <moh94syed@gmail.com>
2022-02-17 07:57:38 +0200

    add quarry to githb

---
+++ lib/screens/splash.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset(
+        'assets/images/splash.jpg',
+        fit: BoxFit.cover,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+      ),
+    );
+  }
+}
